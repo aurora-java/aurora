9191e7595399d34e5b2ef1d11b71b059d1e235bd
bobbie.zou <bobbie.zou@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-04-30 03:08:12 +0000

    删除commit rollback

--- aurora/application/features/AbstractProcedureInvoker.java
+++ aurora/application/features/AbstractProcedureInvoker.java
@@ -4,18 +4,14 @@
  */
 package aurora.application.features;
 
-import java.sql.Connection;
-
-import javax.sql.DataSource;
-
+import java.sql.SQLException;
 import uncertain.composite.CompositeMap;
 import uncertain.ocm.IObjectRegistry;
 import uncertain.ocm.OCManager;
 import uncertain.proc.Procedure;
 import uncertain.proc.ProcedureConfigManager;
 import uncertain.proc.ProcedureRunner;
-import aurora.database.DBUtil;
-import aurora.service.ServiceContext;
+import aurora.database.service.SqlServiceContext;
 
 /**
  * Base class for feature that invoke a procedure at certain action point
@@ -32,54 +28,26 @@ public abstract class AbstractProcedureInvoker {
 
     OCManager           mOcManager;
     IObjectRegistry     mRegistry;
-    DataSource          mDataSource;
-    Connection          mConnection;
+    ProcedureRunner	    mRunner;
+
     
-    /** @todo Use JTA instead */
-    void beginTransaction( ServiceContext context ) throws Exception {
-        mConnection = (Connection)context.getInstanceOfType(Connection.class);
-        if(mConnection==null){
-            mDataSource = (DataSource)mRegistry.getInstanceOfType(DataSource.class);
-            if(mDataSource==null)
-                throw new IllegalStateException("No DataSource instance configured in engine");
-            mConnection = mDataSource.getConnection();
-            context.setInstanceOfType(Connection.class, mConnection);
-        }
-    }
-    
-    void rollbackTransaction( ServiceContext context ) throws Exception {
-        if(mConnection!=null)
-            mConnection.rollback();
-    }
-    
-    void commitTransaction( ServiceContext context ) throws Exception {
-        if(mConnection!=null)
-            mConnection.commit();
-    }
-    
-    void clearUp(){
-        if(mConnection!=null)
-            DBUtil.closeConnection(mConnection);
+    void clearUp() throws SQLException{
+    	if(mRunner!=null)
+    		SqlServiceContext.createSqlServiceContext(mRunner.getContext()).freeConnection();
     }
 
     public void runProcedure(CompositeMap config, ProcedureRunner parent_runner)
             throws Exception {
-        ServiceContext svc_context = ServiceContext.createServiceContext(parent_runner.getContext());
-        beginTransaction(svc_context);
         try{
             CompositeMap proc_config = ProcedureConfigManager.createConfigNode("procedure");
             proc_config.copy(config);
             Procedure proc = (Procedure)mOcManager.createObject(proc_config);
             ProcedureRunner runner = parent_runner.spawn(proc);
             runner.run();
-            if(runner.getException()!=null){
-                rollbackTransaction(svc_context);
-            }
             runner.checkAndThrow();
-            commitTransaction(svc_context);
+            mRunner=runner;
         } finally{
             clearUp();
         }
     }
-
 }
