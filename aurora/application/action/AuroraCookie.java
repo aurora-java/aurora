234aaed3b46eaa6f5b4653c1c262914a2ac97e52
mark.chinama <mark.chinama@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-05-05 05:43:56 +0000

    cookie

---
+++ aurora/application/action/AuroraCookie.java
@@ -0,0 +1,48 @@
+package aurora.application.action;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import aurora.service.ServiceInstance;
+import aurora.service.http.HttpServiceInstance;
+import uncertain.composite.CompositeMap;
+import uncertain.composite.TextParser;
+import uncertain.core.ConfigurationError;
+import uncertain.proc.AbstractEntry;
+import uncertain.proc.ProcedureRunner;
+
+public class AuroraCookie extends AbstractEntry {
+
+	private String name;
+	private String value;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public void run(ProcedureRunner runner) throws Exception {
+
+		CompositeMap mContext = runner.getContext();
+		HttpServiceInstance mService = (HttpServiceInstance) ServiceInstance
+				.getInstance(mContext);
+		HttpServletResponse response = mService.getResponse();
+		this.setValue  (TextParser.parse(this.getValue(), mContext));
+		Cookie cookie = new Cookie(name, value);
+
+		response.addCookie(cookie);
+
+	}
+
+}
