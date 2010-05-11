9463450c8d07396c6bdb76f9b869204410c55f8b
seacat.zhou@gmail.com <seacat.zhou@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-05-11 08:10:39 +0000

    fix returning into statement generation bug

--- aurora/database/local/oracle/sql/OracleSqlBuilder.java
+++ aurora/database/local/oracle/sql/OracleSqlBuilder.java
@@ -6,17 +6,16 @@ package aurora.database.local.oracle.sql;
 
 import java.util.Iterator;
 
-import aurora.database.profile.ISqlBuilder;
 import aurora.database.profile.ISqlBuilderRegistry;
 import aurora.database.sql.FieldWithSource;
 import aurora.database.sql.ISqlStatement;
 import aurora.database.sql.InsertStatement;
+import aurora.database.sql.builder.AbstractSqlBuilder;
 import aurora.database.sql.builder.DefaultInsertBuilder;
 
 /** Build oracle specific sql */
-public class OracleSqlBuilder implements ISqlBuilder {
+public class OracleSqlBuilder extends AbstractSqlBuilder {
     
-    ISqlBuilderRegistry                     mRegistry;
     DefaultInsertBuilder           mInsertBuilder = new DefaultInsertBuilder();
     
     public String createSql( ReturningIntoStatement stmt ){
@@ -31,14 +30,14 @@ public class OracleSqlBuilder implements ISqlBuilder {
                 into_target.append(",");
             }
             fields.append(field.getFieldName());
-            fields.append(mRegistry.getSql(field.getUpdateSource()));
+            into_target.append(mRegistry.getSql(field.getUpdateSource()));
             i++;
         }
         if(i==0)
             throw new IllegalArgumentException("ReturningIntoStatement doesn't contain any field");
-        sql.append(" returning ");
+        sql.append(" RETURNING ");
         sql.append(fields.toString());
-        sql.append(" into ");
+        sql.append(" INTO ");
         sql.append(into_target.toString());
         return sql.toString();
     }
@@ -56,14 +55,17 @@ public class OracleSqlBuilder implements ISqlBuilder {
     public String createSql(ISqlStatement sqlStatement) {
         if(sqlStatement instanceof ReturningIntoStatement)
             return createSql((ReturningIntoStatement)sqlStatement);
+        else if(sqlStatement instanceof OracleInsertStatement)
+            return createSql((OracleInsertStatement)sqlStatement);
         return null;
     }
 
     public void setRegistry(ISqlBuilderRegistry registry) {
-        mRegistry = registry;
-        registerOracleSql();
+        super.setRegistry(registry);
+        mInsertBuilder.setRegistry(registry);        
+        //registerOracleSql();
     }
-    
+
     void registerOracleSql(){
         mRegistry.registerSqlBuilder( ReturningIntoStatement.class, this);
         mRegistry.registerSqlBuilder( OracleInsertStatement.class, this);
