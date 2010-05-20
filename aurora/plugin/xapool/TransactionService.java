b2ea683ddd45fb40de0397da21d0fae6c16900e2
bobbie.zou <bobbie.zou@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-05-20 02:23:27 +0000

    增加自定义transactionManager

--- aurora/plugin/xapool/TransactionService.java
+++ aurora/plugin/xapool/TransactionService.java
@@ -5,22 +5,34 @@ import javax.transaction.UserTransaction;
 import org.objectweb.jotm.Jotm;
 
 import aurora.transaction.ITransactionService;
+import aurora.transaction.UserTransactionImp;
 
 public class TransactionService implements ITransactionService{
 	Jotm jotm=null;
-	public TransactionService() throws NamingException{
-		jotm=new Jotm(true,false);
+	UserTransaction trans=null;	
+	TransactionManager transactionManager=null;
+	public TransactionService(boolean useTransactionManager) throws NamingException{
+		if(useTransactionManager){
+			jotm=new Jotm(true,false);
+			trans=jotm.getUserTransaction();
+			transactionManager=jotm.getTransactionManager();
+		}else{
+			trans=new UserTransactionImp();
+		}
 	}
 	public TransactionManager getTransactionManager() {
-		return jotm.getTransactionManager();
+		return transactionManager;
 	}
 
-	public UserTransaction getUserTransaction() {
-		
-		return jotm.getUserTransaction();
+	public UserTransaction getUserTransaction() {		
+		return trans;
 	}
 
 	public void stop() {
-		jotm.stop();		
+		if(jotm!=null)
+			jotm.stop();
+		if(trans instanceof UserTransactionImp){
+			trans=null;
+		}
 	}
 }
