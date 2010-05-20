c0d3f59b9b4fc0eafac2af5c726347c552b55d6f
bobbie.zou <bobbie.zou@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-05-20 02:24:35 +0000

    自定义UserTransaction实现

---
+++ aurora/transaction/UserTransactionImp.java
@@ -0,0 +1,47 @@
+package aurora.transaction;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+
+import aurora.database.service.SqlServiceContext;
+import aurora.service.IService;
+
+public class UserTransactionImp implements UserTransaction{
+	Connection mConn;		
+	public void initialize(IService svc){		
+		SqlServiceContext context = (SqlServiceContext) svc
+		.getServiceContext().castTo(SqlServiceContext.class);
+		this.mConn=context.getConnection();
+	}
+	public void commit() throws RollbackException, HeuristicMixedException,
+			HeuristicRollbackException, SecurityException,
+			IllegalStateException, SystemException {		
+		try {
+			mConn.commit();
+		} catch (SQLException e) {			
+			throw new IllegalStateException(e);
+		}
+	}
+	public void rollback() throws IllegalStateException, SecurityException,
+	SystemException {
+		try {
+			mConn.rollback();
+		} catch (SQLException e) {		
+			throw new IllegalStateException(e);
+		}
+	}
+	public int getStatus() throws SystemException {
+		return 0;
+	}	
+	public void begin() throws NotSupportedException, SystemException {}
+	public void setRollbackOnly() throws IllegalStateException, SystemException {}
+	public void setTransactionTimeout(int arg0) throws SystemException {}
+
+}
