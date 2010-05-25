70823d2b3e69b7f5de2f001be4c1ba5f071b1445
bobbie.zou <bobbie.zou@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-05-25 09:02:53 +0000

    标记删除

---
+++ aurora/database/features/TagDeleteField.java
@@ -0,0 +1,36 @@
+package aurora.database.features;
+
+import aurora.bm.AbstractSqlCreator;
+import aurora.bm.BusinessModel;
+import aurora.database.service.BusinessModelServiceContext;
+import aurora.database.sql.UpdateStatement;
+
+public class TagDeleteField{
+	String expression;
+	String name;	
+	public String getExpression() {
+		return expression;
+	}
+	public void setExpression(String expression) {
+		this.expression = expression;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}	
+	public void onDecideDeleteStatement(BusinessModelServiceContext context){		
+		context.put("SqlStatementType", new StringBuffer("TagDelete"));
+	}
+	public void onCreateTagDeleteStatement(BusinessModel model,BusinessModelServiceContext context){
+		UpdateStatement stmt = new UpdateStatement(model.getBaseTable(), model.getAlias());
+		stmt.addUpdateField(name, expression);
+		String type = context.getObjectContext().getString("UpdateType", "PK");
+        if("PK".equals(type)){
+        	AbstractSqlCreator.addPrimaryKeyQuery(model, stmt);
+        }
+        context.setStatement(stmt);
+        context.put("SqlStatementType", new StringBuffer("Update"));
+	} 	
+}
