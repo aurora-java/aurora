8c093b6904163527ea53415b1ff6533323f8cb38
seacat.zhou@gmail.com <seacat.zhou@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-05-27 04:54:36 +0000

    stop parent is child has exception

--- aurora/application/features/AbstractProcedureInvoker.java
+++ aurora/application/features/AbstractProcedureInvoker.java
@@ -3,7 +3,9 @@
  * Author: Zhou Fan
  */
 package aurora.application.features;
+import aurora.service.ServiceContext;
 import uncertain.composite.CompositeMap;
+import uncertain.logging.ILogger;
 import uncertain.ocm.IObjectRegistry;
 import uncertain.ocm.OCManager;
 import uncertain.proc.Procedure;
@@ -28,8 +30,20 @@ public abstract class AbstractProcedureInvoker {
             throws Exception {
         CompositeMap proc_config = ProcedureConfigManager.createConfigNode("procedure");
         proc_config.copy(config);
+        /*
         Procedure proc = (Procedure)mOcManager.createObject(proc_config);   
         parent_runner.call(proc);
         parent_runner.checkAndThrow();
+        */
+        Procedure proc = (Procedure)mOcManager.createObject(proc_config);        
+        ProcedureRunner runner = parent_runner.spawn(proc);
+        runner.run();
+        //ILogger logger = parent_runner.getLogger();
+        ServiceContext sc = ServiceContext.createServiceContext(parent_runner.getContext());
+        runner.checkAndThrow();
+        if(!sc.isSuccess()){
+            //logger.severe("success is false");
+            parent_runner.stop();
+        }        
     }
 }
