0e72bf37d2442c497ddfbd035d76d22277c3136c
njq.niu@gmail.com <njq.niu@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-08-13 05:31:50 +0000

    增加errorCode

--- aurora/service/exception/ImageValidationExceptionDescriptor.java
+++ aurora/service/exception/ImageValidationExceptionDescriptor.java
@@ -6,14 +6,18 @@ import aurora.service.ServiceContext;
 import aurora.service.validation.ErrorMessage;
 import aurora.service.validation.ImageValidationException;
 
-
-
+/**
+ * 
+ * @version $Id: ImageValidationExceptionDescriptor.java v 1.0 2010-8-13 下午01:31:24 IBM Exp $
+ * @author <a href="mailto:njq.niu@hand-china.com">vincent</a>
+ */
 public class ImageValidationExceptionDescriptor extends BaseExceptionDescriptor {
 	
 	public CompositeMap process( ServiceContext context, Throwable exception ) {
         ImageValidationException vexp = (ImageValidationException)exception;
         CompositeMap error = super.process(context, exception);
         ErrorMessage msg = (ErrorMessage)DynamicObject.cast(error, ErrorMessage.class);
+        msg.setCode("img_code_error");
         msg.setMessage(vexp.getMessage());
         return error;
     }
