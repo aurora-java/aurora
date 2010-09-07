79c9a4ed9a71e1cf8c944f72762a57aba72abd76
seacat.zhou@gmail.com <seacat.zhou@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-09-07 10:15:45 +0000

    更新模式增加参数，可以只更新传递了参数的字段，支持null参数

--- aurora/service/http/AutoCrudServlet.java
+++ aurora/service/http/AutoCrudServlet.java
@@ -14,11 +14,23 @@ import aurora.application.config.ScreenConfig;
 import aurora.database.actions.config.ActionConfigManager;
 import aurora.database.actions.config.ModelQueryConfig;
 import aurora.database.service.BusinessModelService;
+import aurora.database.service.ServiceOption;
 import aurora.events.E_CheckBMAccess;
 import aurora.service.IService;
+import aurora.service.ServiceContext;
 import aurora.service.controller.ControllerProcedures;
 
 public class AutoCrudServlet extends AbstractAutoServiceServlet {
+    
+    public static final String KEY_UPDATE_PASSED_FIELD_ONLY = "_"+ServiceOption.KEY_UPDATE_PASSED_FIELD_ONLY;
+    
+    private void prepareUpdateAction(ServiceContext context, CompositeMap action_config ){
+        boolean is_update_passed_fields = true;
+        if("false".equalsIgnoreCase(context.getParameter().getString(KEY_UPDATE_PASSED_FIELD_ONLY)))
+            is_update_passed_fields = false;
+        action_config.putBoolean(ServiceOption.KEY_UPDATE_PASSED_FIELD_ONLY, is_update_passed_fields);
+        
+    }
     
     protected void populateService(HttpServletRequest request,
             HttpServletResponse response, IService service) throws Exception {
@@ -27,6 +39,9 @@ public class AutoCrudServlet extends AbstractAutoServiceServlet {
         svc.getController().setProcedureName(
                 ControllerProcedures.INVOKE_SERVICE);
         String uri = request.getRequestURI();
+        if(uri.startsWith("/")){
+            uri = uri.substring(1);
+        }
         String[] args = uri.split("/");
         if (args.length < 4)
             throw new ServletException("Invalid request format");
@@ -57,18 +72,21 @@ public class AutoCrudServlet extends AbstractAutoServiceServlet {
             service_output.put("output", "/model/"+mq.getRootPath());
         } else if("update".equals(action_name)){
             action_config = ActionConfigManager.createModelUpdate(object_name);
+            prepareUpdateAction(svc.getServiceContext(), action_config);
         } else if("insert".equals(action_name)){
             action_config = ActionConfigManager.createModelInsert(object_name);
         } else if("delete".equals(action_name)){
             action_config = ActionConfigManager.createModelDelete(object_name);
         } else if("batch_update".equals(action_name)){
             action_config = ActionConfigManager.createModelBatchUpdate(object_name);
+            prepareUpdateAction(svc.getServiceContext(), action_config);
         } else if("execute".equals(action_name)){
             action_config = ActionConfigManager.createModelAction("model-execute", object_name);
         }
         else
             throw new ServletException("Unknown command:"+action_name);
         screen.getInitProcedureConfig().addChild(0, action_config);
+        System.out.println(action_config.toXML());
         svc.setServiceConfigData(service_config);
     }
 
