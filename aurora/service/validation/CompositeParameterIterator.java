900fe6b906ec1ca8c8e04ad0b5db88c67a8d86ef
seacat.zhou@gmail.com <seacat.zhou@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-09-08 06:11:57 +0000

    Add CompositeParameterIterator

---
+++ aurora/service/validation/CompositeParameterIterator.java
@@ -0,0 +1,65 @@
+/*
+ * Created on 2010-9-8 下午01:12:42
+ * $Id$
+ */
+package aurora.service.validation;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Accept a collection of IParameterIterator and iterates each
+ * CompositeParameterIterator
+ */
+public class CompositeParameterIterator implements IParameterIterator {
+    
+    /**
+     * @param parameterList
+     */
+    public CompositeParameterIterator(Collection parameterList) {
+        if(parameterList==null)
+            throw new NullPointerException("Collection is null");        
+        mParameterList = parameterList;
+        mListIterator = parameterList.iterator();
+        moveNext();
+    }
+
+    Collection              mParameterList;
+    Iterator                mListIterator;
+    IParameterIterator      mCurrentIterator;
+    
+    
+    private void moveNext(){
+        if(mListIterator.hasNext())
+            mCurrentIterator = (IParameterIterator)mListIterator.next();
+        else
+            mCurrentIterator = null;
+    }
+    
+    public void goFirst(){
+        mListIterator = mParameterList.iterator();
+        moveNext();
+    }
+
+    public boolean hasNext() {
+        if(mCurrentIterator==null)
+            return false;
+        if(!mCurrentIterator.hasNext()){
+            moveNext();
+            return hasNext();
+        }
+        return  mCurrentIterator.hasNext();     
+    }
+
+    public IParameter next() {
+        if(mCurrentIterator!=null){
+            IParameter param = mCurrentIterator.next();
+            if(!mCurrentIterator.hasNext())
+                moveNext();
+            return param;
+        }
+        else
+            return null;
+    }
+
+}
