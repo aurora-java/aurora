c38966ca1d437fd47d02c33fb3e6b121bbca4f22
Hugh.HZ.Wu <Hugh.HZ.Wu@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-11-29 09:10:47 +0000

    1)DatePicker增加时间以及多视图功能，新增参数:format,viewsize; demo:AuroraUI/demo/demo/datepicker.screen 2)Aurora.dateFormat增加parseDate等方法。 3)优化Menu弹出时的定位。

--- aurora/presentation/component/std/DatePicker.java
+++ aurora/presentation/component/std/DatePicker.java
@@ -3,6 +3,7 @@ package aurora.presentation.component.std;
 import java.io.IOException;
 import java.util.Map;
 
+import uncertain.composite.CompositeMap;
 import aurora.presentation.BuildSession;
 import aurora.presentation.ViewContext;
 import aurora.presentation.component.std.config.ComponentConfig;
@@ -17,15 +18,20 @@ import aurora.presentation.component.std.config.InputFieldConfig;
 public class DatePicker extends TextField {
 	
 	private static final String PROPERTITY_POPWIDTH = "popwidth";
+	private static final String PROPERTITY_DAY_RENDERER = "dayrenderer";
+	private static final String PROPERTITY_FORMAT = "format";
+	private static final String PROPERTITY_VIEW_SIZE = "viewsize";
 
 	public void onCreateViewContent(BuildSession session, ViewContext context) throws IOException {
 		super.onCreateViewContent(session, context);
 		Map map = context.getMap();
-		
+		CompositeMap view = context.getView();
 		Integer width = (Integer)map.get(ComponentConfig.PROPERTITY_WIDTH);
 		map.put(InputFieldConfig.PROPERTITY_INPUTWIDTH, new Integer(width.intValue()-23));
 		map.put(PROPERTITY_POPWIDTH,new Integer(width.intValue()-2));
-			
+		if(null!=view.getString(PROPERTITY_DAY_RENDERER))addConfig(PROPERTITY_DAY_RENDERER, view.getString(PROPERTITY_DAY_RENDERER));
+		if(null!=view.getString(PROPERTITY_FORMAT))addConfig(PROPERTITY_FORMAT, view.getString(PROPERTITY_FORMAT));
+		if(null!=view.getString(PROPERTITY_VIEW_SIZE))addConfig(PROPERTITY_VIEW_SIZE, view.getString(PROPERTITY_VIEW_SIZE));
 		map.put(CONFIG, getConfigString());
 	}
 }
