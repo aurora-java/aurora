ef0046e38c067c8ce57dc5b5fb3ff5fc73fde047
seacat.zhou@gmail.com <seacat.zhou@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2011-07-02 15:03:24 +0000

    inherit BaseRuntimeException

--- aurora/presentation/ViewCreationException.java
+++ aurora/presentation/ViewCreationException.java
@@ -3,17 +3,19 @@
  */
 package aurora.presentation;
 
+import uncertain.composite.CompositeMap;
+import uncertain.exception.BaseException;
+import uncertain.util.resource.ILocatable;
+import uncertain.util.resource.Location;
+
 /**
  * Exception when creating a view
  * ViewCreationException
  * @author Zhou Fan
  *
  */
-public class ViewCreationException extends Exception {
+public class ViewCreationException extends BaseException {
 
-    public ViewCreationException() {
-        super();
-    }
 
     public ViewCreationException(String message) {
         super(message);
@@ -26,5 +28,50 @@ public class ViewCreationException extends Exception {
     public ViewCreationException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    /**
+     * @param code
+     * @param args
+     * @param location
+     */
+    public ViewCreationException(String code, Object[] args, ILocatable location) {
+        super(code, args, location);
+    }
+
+    /**
+     * @param code
+     * @param args
+     * @param cause
+     * @param config
+     */
+    public ViewCreationException(String code, Object[] args, Throwable cause,
+            CompositeMap config) {
+        super(code, args, cause, config);
+    }
+
+    /**
+     * @param code
+     * @param args
+     * @param cause
+     * @param location
+     */
+    public ViewCreationException(String code, Object[] args, Throwable cause,
+            ILocatable location) {
+        super(code, args, cause, location);
+    }
+
+    /**
+     * @param code
+     * @param args
+     * @param cause
+     * @param source
+     * @param location
+     */
+    public ViewCreationException(String code, Object[] args, Throwable cause,
+            String source, Location location) {
+        super(code, args, cause, source, location);
+    }
+    
+    
 
 }
