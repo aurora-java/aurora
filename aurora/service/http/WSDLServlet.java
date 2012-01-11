f9ae989e99c186680aa993c267a7cca5416fff16
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-01-11 07:09:57 +0000

    add wsdl generator

---
+++ aurora/service/http/WSDLServlet.java
@@ -0,0 +1,112 @@
+package aurora.service.http;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import uncertain.composite.XMLOutputter;
+import uncertain.core.UncertainEngine;
+import uncertain.ocm.IObjectRegistry;
+import aurora.bm.BusinessModel;
+import aurora.bm.Operation;
+import aurora.database.service.DatabaseServiceFactory;
+import aurora.service.ws.WSDLGenerator;
+
+public class WSDLServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static final String SERVLET_NAME = "wsdl";
+	DatabaseServiceFactory mDatabaseServiceFactory;
+
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		ServletContext context = config.getServletContext();
+		UncertainEngine uncertainEngine = WebContextInit.getUncertainEngine(context);
+		if (uncertainEngine == null)
+			throw new ServletException("Uncertain engine not initialized");
+
+		// get global service config
+		IObjectRegistry reg = uncertainEngine.getObjectRegistry();
+		if (reg == null)
+			throw new ServletException("IObjectRegistry not initialized");
+		mDatabaseServiceFactory = (DatabaseServiceFactory) reg.getInstanceOfType(DatabaseServiceFactory.class);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		String[] args = uri.split("/");
+		if (args.length < 4) {
+			throw new ServletException("Invalid request format");
+		}
+		int start_index = 0;
+		for (int i = 0; i < args.length; i++) {
+			String tmp = args[i];
+			if (SERVLET_NAME.equals(tmp)) {
+				start_index = i;
+				break;
+			}
+		}
+		String object_name = args[(start_index + 1)];
+		String operation_name = null;
+		if (start_index + 2 < args.length)
+			operation_name = args[(start_index + 2)];
+
+		BusinessModel bm = mDatabaseServiceFactory.getModelFactory().getModelForRead(object_name);
+		if (bm == null)
+			throw new ServletException("Can't load model:" + object_name);
+		WSDLGenerator wsdl;
+		String fullUrl = getFullUrl(request);
+		if (operation_name == null) {
+			wsdl = new WSDLGenerator(bm, fullUrl);
+		} else if (Operation.QUERY.equalsIgnoreCase(operation_name)) {
+			String multi = request.getParameter("multi_flag");
+			if ("Y".equalsIgnoreCase(multi)) {
+				wsdl = new WSDLGenerator(bm, fullUrl, true);
+			} else {
+				wsdl = new WSDLGenerator(bm, fullUrl, false);
+			}
+		} else {
+			wsdl = new WSDLGenerator(bm, fullUrl, operation_name);
+		}
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html;charset=UTF-8");// 设置响应的MIME类型。
+		try {
+			out.println("<HTML>");
+			out.println("<BODY>");
+			out.println("<XMP>");
+			String content = XMLOutputter.defaultInstance().toXML(wsdl.run(), true);
+			out.print(content);
+			out.println("</XMP>");
+			out.println("</BODY>");
+			out.println("</HTML>");
+			out.flush();
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+		}
+
+	}
+
+	private String getFullUrl(HttpServletRequest request) {
+		StringBuffer url = request.getRequestURL();
+		if (request.getQueryString() != null) {
+			url.append("?");
+			url.append(request.getQueryString());
+		}
+		return url.toString();
+	}
+}
