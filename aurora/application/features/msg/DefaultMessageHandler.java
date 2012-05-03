5815178860981f2ae2c9b9e81dae9157aea3a602
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-05-03 09:14:29 +0000

    支持调用调用外部类

--- aurora/application/features/msg/DefaultMessageHandler.java
+++ aurora/application/features/msg/DefaultMessageHandler.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import java.util.logging.Level;
 
 import uncertain.composite.CompositeMap;
+import uncertain.core.UncertainEngine;
 import uncertain.exception.BuiltinExceptionFactory;
 import uncertain.logging.ILogger;
 import uncertain.logging.LoggingContext;
@@ -16,16 +17,17 @@ import uncertain.proc.Procedure;
 import aurora.service.IServiceFactory;
 import aurora.service.ServiceInvoker;
 
-public class DefaultMessageHandler extends AbstractLocatableObject implements
-		IMessageHandler {
+public class DefaultMessageHandler extends AbstractLocatableObject implements IMessageHandler {
 
 	private IObjectRegistry registry;
-	
+
 	private String name;
 	private String procedure;
-	
+	private String className;
+
 	private IProcedureManager procedureManager;
 	private IServiceFactory serviceFactory;
+	private UncertainEngine uncertainEngine;
 
 	public DefaultMessageHandler(IObjectRegistry registry) {
 		this.registry = registry;
@@ -43,72 +45,75 @@ public class DefaultMessageHandler extends AbstractLocatableObject implements
 	@SuppressWarnings("unchecked")
 	public void onMessage(IMessage message) {
 		ILogger logger = LoggingContext.getLogger(this.getClass().getPackage().getName(), registry);
-
-		logger.log(Level.INFO, "accepted a new message！");
-
-		if (procedureManager == null) {
-			this.procedureManager = (IProcedureManager) registry
-					.getInstanceOfType(IProcedureManager.class);
-			if (procedureManager == null)
-				throw BuiltinExceptionFactory.createInstanceNotFoundException(
-						this, IProcedureManager.class, this.getClass()
-								.getName());
-
-			this.serviceFactory = (IServiceFactory) registry
-					.getInstanceOfType(IServiceFactory.class);
-			if (serviceFactory == null)
-				throw BuiltinExceptionFactory.createInstanceNotFoundException(
-						this, IServiceFactory.class, this.getClass().getName());
-		}
-
-		if (procedure == null) {
-			throw BuiltinExceptionFactory.createAttributeMissing(this,
-					"procedure");
-		}
-
+		logger.log(Level.INFO, "accepted a new message!");
 		if (message == null) {
 			logger.log(Level.WARNING, "message is null");
 			return;
 		}
-			CompositeMap context = new CompositeMap();
+		if (procedure == null && className == null) {
+			throw BuiltinExceptionFactory.createAttributeMissing(this, "procedure,className");
+		}
+		if (procedure != null && className != null) {
+			throw BuiltinExceptionFactory.createConflictAttributesExcepiton(this, "procedure,className");
+		}
 
+		if (procedure != null) {
+			if (procedureManager == null) {
+				this.procedureManager = (IProcedureManager) registry.getInstanceOfType(IProcedureManager.class);
+				if (procedureManager == null)
+					throw BuiltinExceptionFactory.createInstanceNotFoundException(this, IProcedureManager.class, this.getClass().getName());
+
+				this.serviceFactory = (IServiceFactory) registry.getInstanceOfType(IServiceFactory.class);
+				if (serviceFactory == null)
+					throw BuiltinExceptionFactory.createInstanceNotFoundException(this, IServiceFactory.class, this.getClass().getName());
+			}
+			CompositeMap context = new CompositeMap();
 			try {
 				CompositeMap properties = message.getProperties();
-				if(properties != null && !properties.isEmpty()){
+				if (properties != null && !properties.isEmpty()) {
 					Set entrySet = properties.entrySet();
-					for(Iterator<Entry> it = entrySet.iterator();it.hasNext();){
+					for (Iterator<Entry> it = entrySet.iterator(); it.hasNext();) {
 						Entry entry = it.next();
 						// 将解析后的部分放入service的数据容器
-						context.putObject("/parameter/message/@" + entry.getKey(),entry.getValue(), true);
+						context.putObject("/parameter/message/@" + entry.getKey(), entry.getValue(), true);
 					}
 				}
 			} catch (Exception e) {
 				logger.log(Level.SEVERE, "Error when handle message properties ", e);
 			}
-			
 
 			try {
-				logger.log(Level.CONFIG, "receive message text:{0}",
-						new Object[] { message.getText() });
-				logger.log(Level.CONFIG, "load procedure:{0}",
-						new Object[] { procedure });
+				logger.log(Level.CONFIG, "receive message text:{0}", new Object[] { message.getText() });
+				logger.log(Level.CONFIG, "load procedure:{0}", new Object[] { procedure });
 				Procedure proc = null;
 				try {
 					proc = procedureManager.loadProcedure(procedure);
 				} catch (Exception ex) {
-					throw BuiltinExceptionFactory.createResourceLoadException(
-							this, procedure, ex);
+					throw BuiltinExceptionFactory.createResourceLoadException(this, procedure, ex);
 				}
 				String name = "MSG." + procedure;
-				ServiceInvoker.invokeProcedureWithTransaction(name, proc,
-						serviceFactory, context);
+				ServiceInvoker.invokeProcedureWithTransaction(name, proc, serviceFactory, context);
 
 			} catch (Exception ex) {
-				logger.log(Level.SEVERE, "Error when invoking procedure "
-						+ procedure, ex);
+				logger.log(Level.SEVERE, "Error when invoking procedure " + procedure, ex);
 			} finally {
 				context.clear();
 			}
+		} else {
+			uncertainEngine = (UncertainEngine) registry.getInstanceOfType(UncertainEngine.class);
+			try {
+				Class run_class = Class.forName(className);
+				Object object = uncertainEngine.getObjectCreator().createInstance(run_class);
+				if (object == null)
+					throw new IllegalArgumentException(" Can not create instance for " + run_class);
+				if (!(object instanceof IMessageListener))
+					throw BuiltinExceptionFactory.createInstanceTypeWrongException("", IMessageListener.class, object.getClass());
+				IMessageListener lis = (IMessageListener) object;
+				lis.notice(message);
+			} catch (Exception e) {
+				logger.log(Level.SEVERE, "", e);
+			}
+		}
 	}
 
 	public String getProcedure() {
@@ -118,4 +123,13 @@ public class DefaultMessageHandler extends AbstractLocatableObject implements
 	public void setProcedure(String procedure) {
 		this.procedure = procedure;
 	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
 }
