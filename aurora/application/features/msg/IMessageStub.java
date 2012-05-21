07155f2d07999e40556b50bef63bfffc6aaf5510
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-05-21 01:53:50 +0000

    添加状态字段

--- aurora/application/features/msg/IMessageStub.java
+++ aurora/application/features/msg/IMessageStub.java
@@ -1,7 +1,9 @@
 package aurora.application.features.msg;
 
 public interface IMessageStub {
-	
+	public static final int STOP_STATUS = 0;
+	public static final int STARTING_STATUS = 1;
+	public static final int STARTED_STATUS = 2;
 	public boolean isStarted();
 	public IConsumer getConsumer(String topic);
 	public IMessageDispatcher getDispatcher();
