41c780c367c721138ae007a55aba68959ddc92fe
jeschou.zsy@gmail.com <jeschou.zsy@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-07-17 03:29:27 +0000

    增加几个基础函数

--- aurora/application/script/engine/aurora-core.js
+++ aurora/application/script/engine/aurora-core.js
@@ -1,3 +1,26 @@
-function model_query(model,resultpath){
-	println('model_query,not implemented yet!');
+function getSession(create){
+	session=ctx.getChild('session');
+	if(!session&&create){
+		session=new CompositeMap('session');
+		ctx.addChild(session);
+	}
+	return session;
+}
+
+function getCookie(create){
+	cook=ctx.getChild('cookie');
+	if(!cook&&create){
+		cook=new CompositeMap('cookie');
+		ctx.addChild(cook);
+	}
+	return cook;
+}
+
+function getRequest(){
+	req=ctx.getChild('request');
+}
+
+
+function raise_app_error(code){
+	throw code;
 }
