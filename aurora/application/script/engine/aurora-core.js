7e6c935d3324fe5b8d51791db53cc80a3622f485
jeschou.zsy@gmail.com <jeschou.zsy@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-08-17 09:10:13 +0000

    增加$instance(),$cache(),$config()

--- aurora/application/script/engine/aurora-core.js
+++ aurora/application/script/engine/aurora-core.js
@@ -16,6 +16,21 @@ function $bm(model,option){
 	return bm
 }
 
+function $instance(cls){
+	return Packages.aurora.application.script.scriptobject.ScriptUtil.getInstanceOfType(cls);
+}
+
+function $cache(name){
+	var cacheF = $instance('uncertain.cache.INamedCacheFactory');
+    return cacheF.getNamedCache(name);
+}
+
+function $config(){
+	importClass(Packages.uncertain.composite.CompositeUtil);
+    var svc = Packages.aurora.service.ServiceInstance.getInstance($ctx.getData())
+    return svc.getServiceConfigData().getRoot();
+}
+
 function md5(data){
 	return String(Packages.aurora.application.util.MD5Util.md5Hex(data));
 }
