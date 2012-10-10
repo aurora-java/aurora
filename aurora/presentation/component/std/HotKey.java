5e9d9e2bd16e5c2cdcb5458de1aaafb29db756fc
hugh.hz.wu@gmail.com <hugh.hz.wu@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-10-10 07:37:47 +0000

    * 新增HotKey组件

--- aurora/presentation/component/std/HotKey.java
+++ aurora/presentation/component/std/HotKey.java
@@ -1,7 +1,10 @@
 package aurora.presentation.component.std;
 
 import java.io.IOException;
+import java.io.Writer;
+import java.util.Iterator;
 
+import uncertain.composite.CompositeMap;
 import uncertain.ocm.ISingleton;
 
 import aurora.presentation.BuildSession;
@@ -11,16 +14,30 @@ import aurora.presentation.ViewCreationException;
 
 public class HotKey extends Component  implements IViewBuilder, ISingleton {
 	
-	public void onCreateViewContent(BuildSession session, ViewContext context) throws IOException {
-		super.onCreateViewContent(session, context);
-	}
-
-	@Override
+	private static final String KEYS = "keys";
+	private static final String PROPERTITY_BIND = "bind";
+	private static final String PROPERTITY_HANDLER = "handler";
+	
 	public void buildView(BuildSession session, ViewContext view_context)
 			throws IOException, ViewCreationException {
+		CompositeMap view = view_context.getView();
+		CompositeMap keys = view.getChild(KEYS);
+		if(null != keys){
+			Iterator it = keys.getChildIterator();
+			if(null!=it){
+				Writer out = session.getWriter();
+				out.write("<script>");
+				while(it.hasNext()){
+					CompositeMap key = (CompositeMap) it.next();
+					String bind = key.getString(PROPERTITY_BIND);
+					String handler = key.getString(PROPERTITY_HANDLER);
+					out.write("$A.HotKey.addHandler('"+bind+"',"+handler+")");
+				}
+				out.write("</script>");
+			}
+		}
 	}
 
-	@Override
 	public String[] getBuildSteps(ViewContext context) {
 		return null;
 	}
