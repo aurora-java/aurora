e7022ce0e77e1992c6f089a335421b35db100f0f
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-12-11 06:44:10 +0000

    把后台报表任务移入plugin包中

--- aurora/application/task/excel/IExcelTask.java
+++
@@ -1,9 +0,0 @@
-package aurora.application.task.excel;
-
-import uncertain.composite.CompositeMap;
-
-public interface IExcelTask {
-	public String getSvc();
-	public String getDir();
-	public CompositeMap getProcedure();
-}
