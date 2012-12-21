871909bf66f0528ebc369d86fd9d6312ced823e6
seacat.zhou@gmail.com <seacat.zhou@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-12-21 05:24:34 +0000

    another method to define sql error message

---
+++ aurora/service/exception/SqlErrorMessage.java
@@ -0,0 +1,80 @@
+/*
+ * Created on 2012-12-20 下午3:26:59
+ * $Id$
+ */
+package aurora.service.exception;
+
+import java.sql.SQLException;
+
+import uncertain.ocm.IObjectRegistry;
+import uncertain.proc.IExceptionHandle;
+import uncertain.proc.ProcedureRunner;
+import aurora.application.util.LanguageUtil;
+import aurora.service.ServiceContext;
+import aurora.service.validation.ErrorMessage;
+
+public class SqlErrorMessage implements IExceptionHandle {
+
+    int code;
+    String message;
+    String translatedCode;
+
+    IObjectRegistry reg;
+
+    public SqlErrorMessage(IObjectRegistry r) {
+        this.reg = r;
+    }
+
+    public SqlErrorMessage() {
+
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTranslatedCode() {
+        return translatedCode;
+    }
+
+    public void setTranslatedCode(String translatedCode) {
+        this.translatedCode = translatedCode;
+    }
+
+    @Override
+    public boolean handleException(ProcedureRunner runner, Throwable exception) {
+        if (!(exception instanceof SQLException))
+            return false;
+        SQLException sex = (SQLException) exception;
+        if (sex.getCause() != null && sex.getCause() instanceof SQLException) {
+            sex = (SQLException) sex.getCause();
+        }
+        int c = sex.getErrorCode();
+        if (c != code)
+            return false;
+
+        String errorCode = translatedCode == null ? Integer.toString(code)
+                : translatedCode;
+        String errorMsg = reg == null ? message : LanguageUtil
+                .getTranslatedMessage(reg, message, runner.getContext());
+        ServiceContext scx = ServiceContext.createServiceContext(runner
+                .getContext());
+        ErrorMessage msg = new ErrorMessage(errorCode, errorMsg, null);
+        scx.setError(msg.getObjectContext());
+        scx.putBoolean("success", false);
+        return true;
+    }
+
+}
