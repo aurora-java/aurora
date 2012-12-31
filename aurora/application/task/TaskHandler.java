f4b85ff9be0ad1e6814a3d35963ebd3311a73b9a
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-12-31 02:45:22 +0000

    添加打印信息

--- aurora/application/task/TaskHandler.java
+++ aurora/application/task/TaskHandler.java
@@ -610,6 +610,7 @@ public class TaskHandler extends AbstractLocatableObject implements ILifeCycle, 
 			} finally {
 				closeConnection(connection);
 			}
+			logger.log(Level.CONFIG, "taskFetcher finished。");
 			return "finished";
 		}
 	}
