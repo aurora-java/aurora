ad8ab594f3ede73312a823b8579b105ca9146e5f
linjinxiao@gmail.com <linjinxiao@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2013-06-09 02:34:24 +0000

    排序字段自动加_display

--- aurora/application/features/cstm/ConfigCustomizationUtil.java
+++ aurora/application/features/cstm/ConfigCustomizationUtil.java
@@ -357,12 +357,13 @@ public class ConfigCustomizationUtil {
 		config_sql.append("        h.customization_header_id, ");
 		config_sql.append("        h.dimension_code, ");
 		config_sql.append("        h.dimension_value, ");
-		config_sql.append("        (select wmsys.wm_concat(g.name) ");
+		config_sql.append("        (select wmsys.wm_concat(g.name||decode(o.editor_type, 'LOV', '_display', 'COMBOBOX', '_display', '')) ");
 		config_sql.append("   		from (select t.name");
-		config_sql.append("  			  from sys_dynamic_grids t");
+		config_sql.append("  			  from sys_dynamic_grids t,sys_business_object_flexfields o");
 		config_sql.append("        		  where t.cmp_id = ? ");
 		config_sql.append("        		    and t.header_id = ? ");
 		config_sql.append("        		    and t.sequence is not null ");
+		config_sql.append("        		    and t.field_id = o.field_id(+)");
 		config_sql.append("       		  order by t.sequence) g) column_names ");
 		config_sql.append("   from sys_dynamic_headers h, sys_service s");
 		config_sql.append("  where s.service_id = h.service_id ");
